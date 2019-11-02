90db83efc60096f7f070bc9f6ac94e956e56a55b
刘顺 <642517492@qq.com>
2019-11-02 23:29:09 +0800

    配置中心客户端新增  注册中心 重试机制

--- config-client/src/main/java/com/example/ConfigClientApplication.java
+++ config-client/src/main/java/com/example/ConfigClientApplication.java
@@ -2,7 +2,11 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableRetry
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigClientApplication {
 
